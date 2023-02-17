fa020f8e91b2c24193fb5579c63b2b5b22e6fd7b
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2023-02-17 14:09:30 +0200

    CODE: fetch done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,40 @@
-
+import {useState, useEffect} from "react"
 
 // Fetch tavaroita here https://jsonplaceholder.typicode.com/posts
 function About() {
-  return (
-    <div>
-      <p>About random text here too</p>
-    </div>
+
+
+  const [data, setData] = useState([])
+
+  useEffect(()=> {
+    let mounted = true
+    async function fetchData(){
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+      const getData = await response.json()
+    
+      if(mounted){
+        setData(getData)
+      }
+    }
+    fetchData()
+    
+    return () =>{
+      mounted=false
+    }             
+
+  },[])
   
+  return (
+
+    <div>
+      <h1>About to fetch something</h1>
+      {data.map((items)=>(
+        <ul key={items.id}>
+          {items.title}
+        </ul>
+      ))}
+    </div>
+    
   )
 }
 
